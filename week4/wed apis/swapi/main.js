4d87981f0ed0db36f3085effa46eca2a29f07991
Hikmah Abodunrin <hikky24.7@gmail.com>
2023-04-26 16:51:58 -0500

    res.data.results[0]

--- week4/wed apis/swapi/main.js
+++ week4/wed apis/swapi/main.js
@@ -1,14 +1,20 @@
 let btn = document.querySelector('button')
+let body = document.querySelector('body')
 
 const btnClicked = (event) => {
     event.preventDefault();
     console.log('Button Clicked')
     axios.get('https://swapi.dev/api/planets/?search=Alderaan')
         .then((res) => {
-            let resident = res.data.results[0].residents
-            for(i=0; i<resident.length; i++){
-                console.log(resident[i])
-                
+            // let resident = res.data.results[0].residents
+            for(i=0; i<res.data.results[0].residents.length; i++){
+                // console.log(resident[i])
+                axios.get(res.data.results[0].residents[i]).then((res) => {
+                    let addThis = document.createElement('h2')
+                    addThis.textContent = res.data.name
+                    body.appendChild(addThis)
+
+                })
             }
         })
 }
