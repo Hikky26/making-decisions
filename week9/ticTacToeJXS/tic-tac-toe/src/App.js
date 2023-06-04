dfa926dcb5b27637dce3e017c25d24dd814fc8ad
Hikmah Abodunrin <hikky24.7@gmail.com>
2023-06-03 22:52:08 -0500

    update

--- week9/ticTacToeJXS/tic-tac-toe/src/App.js
+++ week9/ticTacToeJXS/tic-tac-toe/src/App.js
@@ -1,34 +1,36 @@
-import Square from './Square';
 import './App.css';
 import React, { useState } from 'react';
+import Square from './Square';
 
-const calculateWinner = (arr) => {
+
+const calculateWinner = (squares) => {
   let lines = [
     //dont feel like typing this out again using 0, so i will leave as is and use i+1
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [3,5,7],
-    [1,5,9]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ]
   for(let i = 0; i<lines.length; i++){
     const [a,b,c] = lines[i]
     if(
-      arr[a] && 
-      arr[a] === arr[b]  &&
-      arr[b] === arr[c] 
+      squares[a] && 
+      squares[a] === squares[b]  &&
+      squares[b] === squares[c] 
     ){
-      return `${arr[a]} won!`
+      return `${squares[a]} won!`
     }
   }
   return "Who will win?"
 }
 
-function App(props) {
 
+function App() {
+  
   const [squares, setSquares] = useState(["","","","","","","","","",])
   const [player, setPlayer] = useState(true)
   const handleClick = () => {
@@ -36,28 +38,28 @@ function App(props) {
     setPlayer(true)
   }
 
-  const squareData = () =>{
-    return(
-      <Square
-      squares={squares}
-      setSquares = {setSquares}
-      player = {player}
-      setPlayer = {setPlayer}
-      index = {index}
-      squareVal = {val}
-    />
-    )
-  }
-
   return (
     <div className="App">
+        <span>
+          {calculateWinner(squares)}
+        </span>
       <div className='container'>
-        {squares.map((val, index) => squareData(val,index))}
-        <button onClick={handleClick}>Reset</button>
+        {squares.map((val, index) => {
+          return(
+            <Square
+            key = {index}
+            squares={squares}
+            setSquares = {setSquares}
+            player = {player}
+            setPlayer = {setPlayer}
+            index = {index}
+            squareVal = {val}
+            />
+            )
+          })
+        }
       </div>
-      <span>
-        {calculateWinner(squares)}
-      </span>
+        <button onClick={handleClick}>Reset</button>
     </div>
   );
 }
--- week9/ticTacToeJXS/tic-tac-toe/src/Square.jsx
+++ week9/ticTacToeJXS/tic-tac-toe/src/Square.jsx
@@ -5,20 +5,20 @@ const Square = (props) => {
         if (!props.squareValue) {
             if (props.player){
                 props.squares.splice(props.index, 1, 'X');
-                props.props.setSquares(props.squares);
+                props.setSquares([...props.squares]);
                 props.setPlayer(!props.player);
             }else{
                 props.squares.splice(props.index, 1, "O");
-                props.setSquares(props.squares);
+                props.setSquares([...props.squares]);
                 props.setPlayer(!props.player);
             }
         }
     }
 
-    console.log(props.squares, props.player)
+    // console.log(props.squares, props.player)
     return (
         <div className="square" onClick={handleClick}>
-            {props.squares=== "O" ? <img src='https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png' /> : props.squareValue}
+            {props.squares=== "O" ? <img src='https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png' alt="O" /> : props.squareValue}
         </div>
     )
 }
